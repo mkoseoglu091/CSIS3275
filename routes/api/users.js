0befb66b6cb59b823c4cee730cafaf98036f4c39
mkoseoglu091 <mkoseoglu091@gmail.com>
2022-05-14 17:41:56 -0700

    fromatting fix

--- routes/api/users.js
+++ routes/api/users.js
@@ -23,68 +23,66 @@ router.post('/', [ // this list of checks come from express-validator imported o
     check('email', 'Please include a valid email').isEmail(),
     check('studentID', 'Please enter a valid student ID Number').isNumeric().isLength({ min: 9, max: 9}),
     check('password', 'Please enter a password with 8 or more characters').isLength({ min: 8 })
-], 
-async (req, res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) { // if there are errors
-        return res.status(400).json({ errors: errors.array() }); // 400 - bad request
-    }
-
-    const {name, email, studentID, password} = req.body;
-
-    try {
-        // See if user exists
-        let userEmail = await User.findOne({ email });
-        let userID = await User.findOne({ studentID });
-        if (userEmail || userID) { // if a user is found with inputted email or studentID
-            return res.status(400).json({ errors: [  { msg: 'User already exists' }] });
+    ], 
+    async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) { // if there are errors
+            return res.status(400).json({ errors: errors.array() }); // 400 - bad request
         }
 
-        // Get users gravatar (based on email)
-        const avatar = gravatar.url(email, {
-            s: '200', // size
-            r: 'pg', // rating: pg so no inappropriate images
-            d: 'mm' // if no avatar found use default
-        })
+        const {name, email, studentID, password} = req.body;
 
-        // create an instance of a user
-        let user = new User({
-            name,
-            email,
-            studentID,
-            avatar,
-            password
-        });
-
-        // Encrypt Password
-        const salt = await bcrypt.genSalt(10); // used to hash the password
-        user.password = await bcrypt.hash(password, salt);
-
-        // Save user to database
-        await user.save();
-
-        // Return JsonWebToken (JWT is used for accessing protected pages that require a password)
-        const payload = {
-            user: {
-                id: user.id // this is the automatically generated ID number in MongoDB
+        try {
+            // See if user exists
+            let userEmail = await User.findOne({ email });
+            let userID = await User.findOne({ studentID });
+            if (userEmail || userID) { // if a user is found with inputted email or studentID
+                return res.status(400).json({ errors: [  { msg: 'User already exists' }] });
             }
-        }
 
-        jwt.sign(
-            payload, 
-            config.get('jwtSecret'),
-            { expiresIn: 3600 }, // The user is signed out after 1 hour (3600 seconds)
-            (err, token) => {
-                if(err) throw err;
-                res.json({ token })
+            // Get users gravatar (based on email)
+            const avatar = gravatar.url(email, {
+                s: '200', // size
+                r: 'pg', // rating: pg so no inappropriate images
+                d: 'mm' // if no avatar found use default
+            })
+
+            // create an instance of a user
+            let user = new User({
+                name,
+                email,
+                studentID,
+                avatar,
+                password
             });
 
-    } catch(err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
+            // Encrypt Password
+            const salt = await bcrypt.genSalt(10); // used to hash the password
+            user.password = await bcrypt.hash(password, salt);
 
-   
+            // Save user to database
+            await user.save();
+
+            // Return JsonWebToken (JWT is used for accessing protected pages that require a password)
+            const payload = {
+                user: {
+                    id: user.id // this is the automatically generated ID number in MongoDB
+                }
+            }
+
+            jwt.sign(
+                payload, 
+                config.get('jwtSecret'),
+                { expiresIn: 3600 }, // The user is signed out after 1 hour (3600 seconds)
+                (err, token) => {
+                    if(err) throw err;
+                    res.json({ token })
+                });
+
+        } catch(err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
 });
 
 module.exports = router;
