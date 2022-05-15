7d20f336903119b1d12ff9ea43519eea9c892a20
mkoseoglu091 <mkoseoglu091@gmail.com>
2022-05-14 23:45:23 -0700

    view tasks request added

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -2,10 +2,28 @@
 
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth'); // any request with auth as 2nd param will be private
+const Tasks = require('../../models/Tasks');
+const User = require('../../models/User');
 
-// @route   GET api/tasks
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => res.send('Tasks Route'));
+// @route   GET api/tasks/me
+// @desc    Get current users tasks
+// @access  Private
+router.get('/me', auth, async (req, res) => {
+    try {
+        const tasks = await Tasks.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']); // find the profile of the user who's token is provided
+        
+        // if there is no profile
+        if(!tasks) {
+            return res.status(400).json({ msg: 'There is no tasks for this user' });
+        }
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+// Start Orientation
 
 module.exports = router;
